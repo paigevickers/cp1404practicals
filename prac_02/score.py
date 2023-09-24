d39621a5d7722f695ac8caf5a2949d6d3ded347f
paigevickers <psigelvickers@gmail.com>
2023-09-24 16:04:39 +1000

    Refactor password check program to use passwords

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,13 +2,28 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    classification = "Invalid"
-elif score < 50:
-    classification = "Bad"
-elif score <= 90:
-    classification = "Pass"
-else:
-    classification = "Excellent"
-print(classification)
+import random
+
+
+def main():
+    user_score = float(input("Enter score: "))
+    classification = determine_result(user_score)
+    print(classification)
+    random_score = random.randint(0, 100)
+    classification = determine_result(random_score)
+    print(classification)
+
+
+def determine_result(score):
+    if score < 0 or score > 100:
+        classification = "Invalid"
+    elif score < 50:
+        classification = "Bad"
+    elif score <= 90:
+        classification = "Pass"
+    else:
+        classification = "Excellent"
+    return classification
+
+
+main()
