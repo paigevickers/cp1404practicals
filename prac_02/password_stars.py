912888c2dd52502db3c761ee512105d2330001ec
paigevickers <psigelvickers@gmail.com>
2023-09-24 15:58:10 +1000

    Refactor password check program to use passwords

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,14 +1,21 @@
+"""
+CP1404 Practical 2
+"""
+
+
 def main():
     password = get_password()
     print_stars(password)
 
 
 def print_stars(password):
+    """Print star for every letter in password"""
     for letter in password:
         print("*", end="")
 
 
 def get_password():
+    """Get valid password from user"""
     password = input("Password: ")
     while len(password) < 5:
         print("Invalid password")
