362cfe590843c1d89ddafd7b836ffea2cf1fce89
paigevickers <psigelvickers@gmail.com>
2023-10-07 19:49:57 +1000

    Basic list operations

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,25 @@
+"""
+CP1404/CP5632 Practical
+Basic list operations
+"""
+
+
+def main():
+    numbers = get_list()
+    print(f"The first number is {numbers[0]}\n"
+          f"The last number is {numbers[-1]}\n"
+          f"The smallest number is {min(numbers)}\n"
+          f"The largest number is {max(numbers)}\n"
+          f"The average of the numbers is {sum(numbers)/len(numbers)}")
+
+
+def get_list():
+    """Get 5 numbers from user input and add to list"""
+    numbers = []
+    for i in range(0, 5):
+        number = int(input("Number: "))
+        numbers.append(number)
+    return numbers
+
+
+main()
