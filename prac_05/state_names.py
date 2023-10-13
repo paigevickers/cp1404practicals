63845f18f5fcbd69232e1e76ccfadaf957004061
paigevickers <psigelvickers@gmail.com>
2023-10-13 18:22:56 +1000

    Complete prac 5 state names exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,13 +8,13 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ").upper()
+state_code = input("Enter short state: ").upper()  # make input upper case
 while state_code != "":
     try:
         print(state_code, "is", CODE_TO_NAME[state_code])
-    except KeyError:
+    except KeyError:  # except KeyError when input is not in CODE_TO_NAME
         print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
+    state_code = input("Enter short state: ").upper() # make input upper case
 
 for code, name in CODE_TO_NAME.items():
     print(f"{code:3} is {name}")
