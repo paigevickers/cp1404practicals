dbaf0fda34b7dacd8c7be41625ee71f37da8e1f7
paigevickers <psigelvickers@gmail.com>
2023-10-13 18:44:39 +1000

    Store colour names and codes in a dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,27 @@
+"""
+CP1404/CP5632 Practical
+Intermediate Exercise
+
+"""
+
+
+def main():
+    COLOUR_TO_CODE = {"Acid Green": "b0bf1a", "Banana Mania": "fae7b5",
+                      "Cosmic Cobalt": "2e2d88", "Dogwood Rose": "d71868",
+                      "Electric Lime": "ccff00", "Fuzzy Wuzzy": "87421f",
+                      "Glossy Grape": "ab92b3", "Mango Tango": "ff8243",
+                      "Twilight Lavender": "8a496b", "Vegas Gold": "c5b358"}
+    colour_name = input("Colour name: ").title()
+    while colour_name != "":
+        get_valid_colour(COLOUR_TO_CODE, colour_name)
+        colour_name = input("Colour name: ").title()
+
+
+def get_valid_colour(COLOUR_TO_CODE, colour_name):
+    if colour_name in COLOUR_TO_CODE:
+        print(f"{colour_name} is #{COLOUR_TO_CODE[colour_name]}")
+    else:
+        print("Invalid colour name")
+
+
+main()
