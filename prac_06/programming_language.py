33e68b55622a433832e0fa26f8602e302a69a916
paigevickers <psigelvickers@gmail.com>
2023-10-20 12:09:15 +1000

    Add docstrings

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,10 +1,22 @@
+"""
+CP1404/CP5632 Practical
+Intermediate Exercise - ProgramingLanguage class
+Estimate:
+Actual:
+"""
+
+
 class ProgrammingLanguage:
+    """Represent a programming language object"""
+
     def __init__(self, typing="", reflection="", year=0):
+        """Initialise a ProgrammingLanguage instance"""
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
+        """Determine if the language is dynamic"""
         if self.typing == "Dynamic":
             return True
         else:
