6d07c996b465af8d3da0905b531d775523c64e6d
paigevickers <psigelvickers@gmail.com>
2023-10-20 14:58:12 +1000

    Add comments and docstrings

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -15,15 +15,15 @@ def main():
     longest_name = ""
     largest_cost = 0.0
     while name != "":
-        guitar = get_guitar(name)
-        guitars = get_list(guitars, guitar)
-        print_added_guitar(guitar)
+        guitar = get_guitar(name)  # create Guitar object
+        guitars = get_list(guitars, guitar)  # get list of guitars
+        print_added_guitar(guitar) # added guitar message
         name = input("Name: ")
         for guitar in guitars:
-            longest_name = get_longest_name(guitar, longest_name)
-            largest_cost = get_longest_cost(guitar, largest_cost)
-    print_loading_message()
-    print_guitar_list(guitars, longest_name, largest_cost)
+            longest_name = get_longest_name(guitar, longest_name)  # determine longest guitar name in list
+            largest_cost = get_longest_cost(guitar, largest_cost)  # determine largest cost of guitar in list
+    print_loading_message()  # loading message
+    print_guitar_list(guitars, longest_name, largest_cost)  # display of all guitars inputted
 
 
 def get_longest_cost(guitar, largest_cost):
@@ -45,20 +45,23 @@ def print_guitar_list(guitars, longest_name, longest_cost):
     for guitar in guitars:
         vintage_string = determine_vintage_string(guitar)  # determine if guitar is vintage
         print(f"Guitar {1 + guitars.index(guitar)}: {guitar.name:>{len(longest_name)}}, "
-              f"worth ${guitar.cost:{len(str(longest_cost))},.2f} {vintage_string}")  #
+              f"worth ${guitar.cost:{len(str(longest_cost))},.2f} {vintage_string}")
 
 
 def print_loading_message():
-    print("\n... snip ...\n"
-          "\n"
+    """Print message user will see when done inputting guitars"""
+    print("\n... snip ...\n"  # add blank line before and after message
+          "\n"  # add blank line
           "These are my guitars:")
 
 
 def print_added_guitar(guitar):
+    """Print guitar's information when added"""
     print(f"{guitar.name} ({guitar.year}) : ${guitar.cost} added.")
 
 
 def get_guitar(name):
+    """Create Guitar object"""
     year = int(input("Year: "))
     cost = float(input("Cost: "))
     guitar = Guitar(name, year, cost)
@@ -66,11 +69,13 @@ def get_guitar(name):
 
 
 def get_list(guitars, guitar):
+    """Append guitar to list to get list of guitars"""
     guitars.append(guitar)
     return guitars
 
 
 def determine_vintage_string(guitar):
+    """Determine if a guitar is vintage"""
     if guitar.is_vintage():
         vintage_string = "(vintage)"
     else:
