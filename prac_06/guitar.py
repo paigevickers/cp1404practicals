92833e3015b4b952b52f1a0397bcd1acd34bc7dc
paigevickers <psigelvickers@gmail.com>
2023-10-20 12:46:54 +1000

    Add docstrings

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -7,15 +7,19 @@ Actual: 20 minutes
 
 
 class Guitar:
+    """Represent a Guitar object"""
     def __init__(self, name="", year=0, cost=0.0):
+        """Initialise a Guitar instance"""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Return string of Guitar object"""
         return f"{self.name} ({self.year}) : ${self.cost}"
 
     def is_vintage(self):
+        """Determine if the Guitar object is vintage"""
         if (2023 - self.year) >= 50:
             return True
         else:
