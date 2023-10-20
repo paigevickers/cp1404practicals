bf2fe5f91b87ffe14484f5007769e12867be0d82
paigevickers <psigelvickers@gmail.com>
2023-10-20 11:30:52 +1000

    Add name field to the Car class

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,11 +4,12 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self._odometer = 0
 
@@ -31,4 +32,4 @@ class Car:
         return distance
 
     def __str__(self):
-        return f"Car, fuel={self.fuel}, odometer={self._odometer}"
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
