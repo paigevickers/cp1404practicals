4b768a85854865973c6946cb9042558c2e6c2e59
paigevickers <psigelvickers@gmail.com>
2023-10-20 12:30:09 +1000

    Refactor variable name in class

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -9,9 +9,9 @@ Actual:
 class ProgrammingLanguage:
     """Represent a programming language object"""
 
-    def __init__(self, language="", typing="", reflection="", year=0):
+    def __init__(self, name="", typing="", reflection="", year=0):
         """Initialise a ProgrammingLanguage instance"""
-        self.language = language
+        self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
@@ -24,5 +24,5 @@ class ProgrammingLanguage:
             return False
 
     def __str__(self):
-        return f"{self.language}, {self.typing} Typing, Reflection={self.reflection}, " \
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, " \
                f"First appeared in {self.year}"
