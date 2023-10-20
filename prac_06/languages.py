a4a71fd73f3b85839a184c314fc5f7a914f2989e
paigevickers <psigelvickers@gmail.com>
2023-10-20 12:32:21 +1000

    Create list of objects and loop through for languages with dynamic typing

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -12,6 +12,14 @@ def main():
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
     print(python)
+    programming_languages = [python, ruby, visual_basic]
+    dynamic_languages = [language.name for language in programming_languages if language.is_dynamic()]
+    print(f"The dynamically typed languages are:\n"
+          f"{dynamic_languages[0]}\n"
+          f"{dynamic_languages[1]}")
+
+    # dynamic_languages = [language for language in programming_languages if language.is_dynamic() == "True"]
+    # print(dynamic_languages)
 
 
 main()
