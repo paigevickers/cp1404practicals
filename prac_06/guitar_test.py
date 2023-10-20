0e768008f24dd00a7e9dd2f5f49b76ec94d1b4d0
paigevickers <psigelvickers@gmail.com>
2023-10-20 14:59:19 +1000

    Complete guitar testing

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,8 +1,8 @@
 """
 CP1404/CP5632 Practical
 Do-from-scratch Exercises - Create program to test Guitar class
-Estimate: 15 minutes
-Actual: 10 minutes
+Estimate: 10 minutes
+Actual: 1 minutes
 """
 from guitar import Guitar
 
@@ -10,7 +10,7 @@ from guitar import Guitar
 def main():
     gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
     another_guitar = Guitar("Another Guitar", 2013, 7289.99)
-    print(gibson)  # test class executes correctly
+    print(gibson)  # test class object is correct with correct formatting
     print(another_guitar)
     print(f"{gibson.name} get_age() - Expected 101. Got {gibson.get_age()}")  # test get_age() works correctly
     print(f"{another_guitar.name} get_age() - Expected 10. Got {another_guitar.get_age()}")
