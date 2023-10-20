7c28fe11c83a36cc0dd98a90a02af70f7ca213f1
paigevickers <psigelvickers@gmail.com>
2023-10-20 11:55:54 +1000

    Complete walkthrough example

--- prac_06/car.py
+++ prac_06/car.py
@@ -32,4 +32,4 @@ class Car:
         return distance
 
     def __str__(self):
-        return f"{self.name.title()}, fuel={self.fuel}, odometer={self._odometer}"
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
