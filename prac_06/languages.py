d418e8b3e414ff730de1b0a83f420ce12db8da50
paigevickers <psigelvickers@gmail.com>
2023-10-20 12:34:27 +1000

    Refactor main into separate functions

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -11,15 +11,21 @@ def main():
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-    print(python)
-    programming_languages = [python, ruby, visual_basic]
-    dynamic_languages = [language.name for language in programming_languages if language.is_dynamic()]
+    programming_languages = get_list(python, ruby, visual_basic)
+    dynamic_languages = get_dynamic_languages(programming_languages)
     print(f"The dynamically typed languages are:\n"
           f"{dynamic_languages[0]}\n"
           f"{dynamic_languages[1]}")
 
-    # dynamic_languages = [language for language in programming_languages if language.is_dynamic() == "True"]
-    # print(dynamic_languages)
+
+def get_dynamic_languages(programming_languages):
+    dynamic_languages = [language.name for language in programming_languages if language.is_dynamic()]
+    return dynamic_languages
+
+
+def get_list(python, ruby, visual_basic):
+    programming_languages = [python, ruby, visual_basic]
+    return programming_languages
 
 
 main()
