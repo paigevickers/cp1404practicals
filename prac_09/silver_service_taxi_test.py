07a3b956d81cffe3a3ba9394c6c2acd73ce6affd
paigevickers <psigelvickers@gmail.com>
2023-11-17 12:02:36 +1000

    Run test code on classes

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -4,16 +4,16 @@ Test SilverServiceTaxi class
 """
 from prac_09.silver_service_taxi import SilverServiceTaxi
 
-my_hummer = SilverServiceTaxi("Hummer", 200, 4)  # create SilverServiceTaxi object
-print(my_hummer)
-my_hummer.drive(40)  # drive the taxi 40 km
-print(my_hummer)  # print the taxi's details
-print(my_hummer.get_fare())  # print the taxi's current fair
-my_hummer.start_fare()  # restart meter by starting new fare
-my_hummer.drive(100)  # drive 100km
-print(my_hummer.get_fare())  # print the taxi's new fair
+my_taxi = SilverServiceTaxi(name="Hummer", fuel=200, fanciness=2)  # create silver service taxi object
+my_taxi.drive(70)  # drive the taxi 40 km
+print(my_taxi)  # print the taxi's details
+print(my_taxi.get_fare())  # print the taxi's current fair
+my_taxi.start_fare()  # restart meter by starting new fare
+my_taxi.drive(100)  # drive 100km
+print(my_taxi)
+print(my_taxi.get_fare())
 
-my_rover = SilverServiceTaxi("Range Rover", 100, 2)  # create a SilverServiceTax object
-print(my_rover)
+my_rover = SilverServiceTaxi(name="Range Rover", fuel=100, fanciness=2)  # create a SilverServiceTax object
 my_rover.drive(18)
+print(my_rover)
 print(my_rover.get_fare())
